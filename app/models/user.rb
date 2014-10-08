ee72f924c3f6efd2f5e52fb644faece70ce6a0ad
Prelang Builder <builder@prelang.com>
2014-10-08 14:07:20 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   
   #->Prelang (user_login:devise/username_login_support)
   has_many :pedidos
+  belongs_to :pizza
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
